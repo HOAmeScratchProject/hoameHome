acef1a53d03704c473898df0bdcf40dac75c4c9f
Vasean Annin <vaseanannin@Vaseans-MacBook-Air.local>
2024-10-09 23:01:11 -0400

    setting up dev enviornment

--- setup.sql
+++ setup.sql
@@ -6,12 +6,21 @@ CREATE TABLE IF NOT users (
     phone_number INTEGER UNIQUE,
     role VARCHAR (50),
     email VARCHAR(100) UNIQUE NOT NULL,
-    
+     );
+
+      CREATE TABLE IF NOT user_type (
+    id SERIAL PRIMARY KEY,
+    admin BOOLEAN DEFAULT FALSE,
+    owner BOOLEAN DEFAULT FALSE,
+    board BOOLEAN DEFAULT FALSE,
+    arc BOOLEAN DEFAULT FALSE,
+    roads BOOLEAN DEFAULT FALSE,
      );
 
      CREATE TABLE IF NOT files (
     id SERIAL PRIMARY KEY,
     file_name VARCHAR(100) NOT NULL,
+    file_date BYTEA  NOT NULL, 
     file_size INTEGER NOT NULL,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
      );
