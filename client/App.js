f2a871bb2c40bd054f9279ec82402be677441ee9
Brian Anderson <brianmichealanderson@yahoo.com>
2024-10-14 19:53:49 -0700

    React & Express updates - fetch/get, post/create requests front-end to back-end

--- client/App.js
+++ client/App.js
@@ -1,10 +1,29 @@
-import Dashboard from "./components/Dashboard";
+// import Dashboard from "./components/Dashboard";
+// const App = () => {
+//   return (
+//   <div>
+//       <Dashboard/>
+//   </div>
+//   )
+// };
+
+// export default App;
+
+import React from 'react';
+import Dashboard from "./components/DashBoard";
+
 const App = () => {
+  // Define the logout function
+  const handleLogout = () => {
+    console.log('User logged out');
+    // Implement actual logout functionality here, like clearing user data or redirecting
+  };
+
   return (
-  <div>
-      <Dashboard/>
-  </div>
-  )
+    <div>
+      <Dashboard onLogout={handleLogout} /> {/* Pass the function as a prop */}
+    </div>
+  );
 };
 
 export default App;
