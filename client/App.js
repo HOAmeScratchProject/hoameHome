96ae2204261a2532edcd4740a80cb37b10852259
Vasean Annin <vaseanannin@Vaseans-MacBook-Air.local>
2024-10-16 21:55:06 -0400

    created a working login component that allows the user to access the application upon a successful login

--- client/App.js
+++ client/App.js
@@ -10,10 +10,14 @@
 // export default App;
 
 import React from 'react';
-import Dashboard from "./components/DashBoard";
+import Dashboard from './components/DashBoard';
+import Login from './components/Login';
+import { useState } from 'react';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // Define the logout function
+
   const handleLogout = () => {
     console.log('User logged out');
     // Implement actual logout functionality here, like clearing user data or redirecting
@@ -21,9 +25,25 @@ const App = () => {
 
   return (
     <div>
-      <Dashboard onLogout={handleLogout} /> {/* Pass the function as a prop */}
+      {isLoggedIn ? (
+        <Dashboard onLogout={setIsLoggedIn} />
+      ) : (
+        <Login onLogin={setIsLoggedIn} />
+      )}
     </div>
   );
 };
 
 export default App;
+
+// try {
+//   const response = await fetch(`http://localhost:3000/api/announcements`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       title: newTitle,
+//       message: newMessage,
+//     }),
+//   });
