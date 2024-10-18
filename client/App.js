ff7ccfa5b923411ffebee3498e9e39d2e7e5ad8e
Brian Anderson <brianmichealanderson@yahoo.com>
2024-10-17 19:33:18 -0700

    Resolved App.js merge conflict

--- client/App.js
+++ client/App.js
@@ -1,59 +1,18 @@
-// import Dashboard from "./components/Dashboard";
-// const App = () => {
-//   return (
-//   <div>
-//       <Dashboard/>
-//   </div>
-//   )
-// };
-
-// export default App;
-
 import React from 'react';
-<<<<<<< HEAD
-import Dashboard from "./components/DashBoard";
-// import { useState } from 'react';
-
-const App = () => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false); //login state
-
-
-=======
 import Dashboard from './components/DashBoard';
 import Login from './components/Login';
 import { useState } from 'react';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
->>>>>>> 2e18c88ba4b37816294cebd19577d2d4761e9d59
-  // Define the logout function
-
-  const handleLogout = () => {
-    console.log('User logged out');
-    // Implement actual logout functionality here, like clearing user data or redirecting
-    //////// set isLoggedIn back to false
-  };
-
-  // //conditional rendering of Dashboard container if isLoggedIn = true.
-  // {isLoggedIn ? (
-  //   render Dashboard component
-  // ) : (
-  //   render Login component passing setIsLogged setter function as props
-  // )}
-
 
   return (
     <div>
-<<<<<<< HEAD
-
-      <Dashboard onLogout={handleLogout} /> {/* Pass the function as a prop */}
-=======
       {isLoggedIn ? (
         <Dashboard onLogout={setIsLoggedIn} />
       ) : (
         <Login onLogin={setIsLoggedIn} />
       )}
->>>>>>> 2e18c88ba4b37816294cebd19577d2d4761e9d59
     </div>
   );
 };
@@ -71,3 +30,14 @@ export default App;
 //       message: newMessage,
 //     }),
 //   });
+
+// import Dashboard from "./components/Dashboard";
+// const App = () => {
+//   return (
+//   <div>
+//       <Dashboard/>
+//   </div>
+//   )
+// };
+
+// export default App;
